5be7c043ae286138f09c404a9121263a7867d59b
Bokun Wang 王博坤 <bokun.wang@MBC02W96FNG8WN.lan>
2018-11-06 23:33:14 +0800

    update hex

--- types/hexip.go
+++ types/hexip.go
@@ -7,6 +7,46 @@ import (
 )
 
 
+func HexToIPv4(s string) net.IP {
+
+	ipList := make([]string, 4)
+
+	for i := range ipList {
+
+		part, _ := strconv.ParseInt(s[i*2:i*2+2], 16, 0)
+		ipList[4-1-i] = fmt.Sprintf("%d", part)
+	}
+
+	return net.ParseIP(strings.Join(ipList, "."))
+}
+   
+func HexToIPv6(s string) net.IP {
+
+	ipList := make([]string, 8)
+	for i := range ipList {
+		ipList[i] = s[i*4 : i*4+4]
+	}
+
+	return net.ParseIP(strings.Join(ipList, ":"))
+}
+
+// 方法一
+func HexToIP(s string) net.IP {
+
+	switch len(s) {
+
+	case 8:
+	 	return HexToIPv4(s)
+	case 32:
+	 	return HexToIPv6(s)
+	default:
+		 return s
+		 
+	}
+}
+
+
+//方法二
 func HexToIp(hexstr string)(string){
 	var ip string
 
--- types/strconv.go
+++ types/strconv.go
@@ -5,17 +5,27 @@ import(
 )
 
 func IntToString(i int)(string){
+	
 	return strconv.Itoa(i)
 }
 
 func  Int64ToString(i int64)(string){
+
 	return strconv.FormatInt(i,10)
 }
 
 func StringToInt(s string)(int,error){
+
 	return strconv.Atoi(s)
 }
 
 func StringToint64(s string)(int64,error){
+
 	return  strconv.ParseInt(s, 10, 64)
+}
+
+func Hex2dec(hexstr string) string{
+
+    i, _ := strconv.ParseInt(hexstr, 16, 0)
+    return strconv.FormatInt(i, 10)
 }
