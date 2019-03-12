aacd180ab21585b348facfff6842e8901b1aaf09
Bokun Wang 王博坤 <bokun.wang@MBC02W96FNG8WN.local>
2019-03-12 15:01:46 +0800

    update

--- time/gap.go
+++ time/gap.go
@@ -29,3 +29,28 @@ func Gap(start_time, end_time string) string {
 	}
 	return "Null"
 }
+
+func getHourDiffer(start_time, end_time string) string {
+	// var hour int64
+	t1, err := time.ParseInLocation("2006-01-02 15:04:05", start_time, time.Local)
+	t2, err := time.ParseInLocation("2006-01-02 15:04:05", end_time, time.Local)
+	if err == nil && t1.Before(t2) {
+		diff := t2.Unix() - t1.Unix() 
+
+		if diff/60 < 60 {
+			return fmt.Sprintf("%d分钟", diff/60)
+		}
+
+		if diff/60 > 60 {
+			return fmt.Sprintf("%d小时", diff/60/60)
+		}
+
+		if diff/60 > 1440 {
+			return fmt.Sprintf("%d天", diff/60/60/24)
+		}
+
+	} else {
+		return "0秒"
+	}
+	return "0秒"
+}
