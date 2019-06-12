a1bf04b3555782a68abe8381aad97189eaefc2a1
Bokun Wang 王博坤 <bokun.wang@MBC02W96FNG8WN.local>
2019-06-12 11:14:16 +0800

    update exec

--- exec/exec.go
+++ exec/exec.go
@@ -2,13 +2,14 @@ package exec
 
 import (
 	"os/exec"
+	"strings"
 	"fmt"
 )
 
 
 func Command(command string) (status bool){
-
-	_, err := exec.Command("bash", "-c", command).Output()
+	args := strings.Split(command, " ")
+	_, err := exec.Command(args[0], args[1:]...).Output()
 
 	if err != nil {
 		fmt.Printf("Failed to execute!",err)
