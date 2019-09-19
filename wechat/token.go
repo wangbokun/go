eafabc0cd7a3107a3769cb5d3cd50a2034e272bf
Bokun Wang 王博坤 <bokun.wang@MBC02W96FNG8WN.local>
2019-09-19 16:06:58 +0800

    update http

---
+++ wechat/token.go
@@ -0,0 +1,31 @@
+package wechat
+
+import ( 
+	"github.com/wangbokun/go/http"
+	"github.com/wangbokun/go/types"
+)
+
+const (
+	wechartUrl = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid="
+)
+type Token struct{
+	Errcode 		int 		`json:"errcode"`
+	Errmsg  		string 		`json:"errmsg"`
+	Access_token 	string 		`json:"access_token"`
+	Expires_in 		int 		`json:"expires_in"`
+}
+
+func GetToken(corpid,corpsecret string) (string,error) {
+	Url:=wechartUrl+corpid+"&corpsecret="+corpsecret
+	ctx,err:=http.Get(Url)
+	if err != nil{
+		return "err",err
+	}
+	WechatToken := &Token{}
+
+	err =types.FormatJSON(ctx,WechatToken)
+	if err != nil{
+		return "err",err
+	}
+	return WechatToken.Access_token,nil
+}
