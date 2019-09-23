187b9107b3e29ae4edde527347a3ec33fc3ea4e7
Bokun Wang 王博坤 <bokun.wang@MBC02W96FNG8WN.local>
2019-09-23 17:57:38 +0800

    update

---
+++ template/template.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"bytes"
+	"strings"
+	"text/template"
+)
+// use:
+//     data, err := util.RenderTemplate("templates/xxx.template", &res)
+//    if err != nil {
+// 	   return err
+//    }  
+func RenderTemplate(file string, v interface{}) (string, error) {
+	t, err := template.ParseFiles(file)
+	if err != nil {
+		return "", err
+	}
+	var buf bytes.Buffer
+	err = t.Execute(&buf, v)
+	return buf.String(), err
+}
+
+type Template map[string]interface{}
+
+func (t *Template) Up(s string) string {
+	return strings.ToUpper(s)
+}
